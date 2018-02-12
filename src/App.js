a3f4834019735c06d13f01ed4f820a7905173f01
daniel818 <avital.daniel@gmail.com>
2018-02-12 18:29:54 +0200

    code cleanup + adding comments

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const WINDOW_SIZE = 8;
 const RANGES = [
   { type: "windowRange", name: WINDOW_SIZE },
   { type: "thumbnail", name: THUMBNAIL_RES },
-
   { type: "lowRange", name: LOW_RES },
   { type: "standardRange", name: STANDARD_RES }
 ];
@@ -25,7 +24,11 @@ class App extends Component {
     };
     // bind methods
     this.calculateIndex = this.calculateIndex.bind(this);
+    this.createRanges = this.createRanges.bind(this);
+    this.updateImageRes = this.updateImageRes.bind(this);
+    this.renderImages = this.renderImages.bind(this);
   }
+
   componentDidMount() {
     window.addEventListener("scroll", this.calculateIndex);
   }
@@ -37,29 +40,30 @@ class App extends Component {
    * CalculateIndex:
    * step 1 - position off y axis / width (=height) gives you the index of the image in (0,0)
    * step 2 - calculate the number of images in view total height / image width (=height)
-   * step 3 - divde step 2 result by two to get the center image offset
-   * step 3 - add step 1 and step 3 to find the currect center index
+   * step 3 - divide step 2 result by two to get the center image offset
+   * step 3 - add step 1 and step 3 to find the current center index
    */
   calculateIndex() {
     const activeIndex = Math.floor(
       window.scrollY / window.innerWidth +
         window.innerHeight / window.innerWidth / 2
     );
+
     if (this.state.activeIndex !== activeIndex) {
       this.setState({ activeIndex });
     }
   }
 
   /**
-   * Render The images Resolution by range of each resolution Type
+   * Create an array of ranges for each Resolution and the window
+   * @param ranges
    */
-  renderImages() {
+  createRanges(ranges) {
     const { data } = this.props;
     const { activeIndex } = this.state;
     const length = data.length - 1;
 
-    //Array that hold the type of range and the array of ranges
-    const ranges = RANGES.map(range => {
+    return ranges.map(range => {
       return {
         type: range.type,
         range: _.range(
@@ -69,7 +73,28 @@ class App extends Component {
         )
       };
     });
-    //Destructuring
+  }
+
+  /**
+   *  Iterate over the [type] res range and check if its
+   *  in the window range => if it is update [type] to true
+   * @param array
+   * @param slidingWindow
+   * @param type
+   */
+  updateImageRes(array, slidingWindow, type) {
+    array.forEach(i => {
+      const obj = _.find(slidingWindow, winObj => winObj.index === i);
+      obj[type] = true;
+    });
+  }
+
+  /**
+   * Render The images Resolution by range of each resolution Type
+   */
+  renderImages() {
+    const { data } = this.props;
+    const ranges = this.createRanges(RANGES);
     const {
       windowRange = ranges[0],
       thumRange = ranges[1],
@@ -81,25 +106,31 @@ class App extends Component {
       return { index: i, useColor: true };
     });
 
-    //Itirate over the thumb res range and check if its in the window range => if it is update useThumb to true
-    thumRange.range.forEach(i => {
-      const obj = _.find(slidingWindow, winObj => winObj.index === i);
-      obj.useThumb = true;
-    });
-
-    //Itirate over the low res range and check if its in the window range => if it is update useLow to true
-    lowRange.range.forEach(i => {
-      const obj = _.find(slidingWindow, winObj => winObj.index === i);
-      obj.useLow = true;
-    });
-
-    //Itirate over the stnd res range and check if its in the window range => if it is update useStandard to true
-    standardRange.range.forEach(i => {
-      const obj = _.find(slidingWindow, winObj => winObj.index === i);
-      obj.useStandard = true;
-    });
+    this.updateImageRes(thumRange.range, slidingWindow, "useThumb");
+    this.updateImageRes(lowRange.range, slidingWindow, "useLow");
+    this.updateImageRes(standardRange.range, slidingWindow, "useStandard");
+    // // Iterate over the thumb res range and check if its in the window range => if it is update useThumb to true
+    // thumRange.range.forEach(i => {
+    //   const obj = _.find(slidingWindow, winObj => winObj.index === i);
+    //   obj.useThumb = true;
+    // });
+    //
+    //
+    //
+    // // Iterate over the low res range and check if its in the window range => if it is update useLow to true
+    // lowRange.range.forEach(i => {
+    //   const obj = _.find(slidingWindow, winObj => winObj.index === i);
+    //   obj.useLow = true;
+    // });
+    //
+    // // Iterate over the standard res range and check if its in the window range => if it is , update useStandard to true
+    // standardRange.range.forEach(i => {
+    //   const obj = _.find(slidingWindow, winObj => winObj.index === i);
+    //   obj.useStandard = true;
+    // });
 
     return slidingWindow.map(mapping => {
+
       const { useThumb, useLow, useStandard, index } = mapping;
       const model = data[index];
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import gallery from './gallery-data.json';
+
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(<App data={gallery}/>, document.getElementById('root'));
